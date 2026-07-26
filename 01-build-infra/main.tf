cb80a5217ca3d84734a704c1a92938bfe6b7b4c8
Soumen Bera <bera.soumen5@gmail.com>
2026-07-27 01:57:18 +0530

    04 - s3 backend applied

--- 01-build-infra/main.tf
+++ 01-build-infra/main.tf
@@ -7,14 +7,24 @@ terraform {
   }
   required_version = ">= 1.2"
 }
+terraform {
+  backend "s3" {
+    bucket       = "soumen-tf-learn-2026-8823"
+    key          = "terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
+
 
 provider "aws" {
   region = "ap-south-1"
 }
-resource "aws_instance" "app_server" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  tags = {
-    Name = "learn-terraform"
-  }
-}
+# resource "aws_instance" "app_server" {
+#   ami           = var.ami
+#   instance_type = var.instance_type
+#   tags = {
+#     Name = "learn-terraform"
+#   }
+# }
