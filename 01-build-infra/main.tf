fa5ecff96c5d6e21926d180ccb135a6477bf95aa
Soumen Bera <bera.soumen5@gmail.com>
2026-07-27 00:07:30 +0530

    build infra tested

---
+++ 01-build-infra/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+  required_version = ">= 1.2"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_instance" "app_server" {
+  ami           = "ami-09d88f7c4c272b0c5"
+  instance_type = "t3.small"
+  tags = {
+    Name = "learn-terraform"
+  }
+}
